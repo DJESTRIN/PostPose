24c0bfb8551682e09a7284372986f04776d871d0
David <david.james.estrin@outlook.com>
2024-09-04 16:36:16 -0400

    kenneth script added

--- core/kenneth_heatmap.py
+++ core/kenneth_heatmap.py
@@ -1,5 +1,10 @@
-def plot_heatmap(image,trajectory):
-        # Take method from plot_trajectory,
-        # bin the pixels for the heatmap
-        # plot heatmap instead of the line to show where the mouse generally was located on screen
+
+def plot_heatmap(image,trajectory,outputfolder): #Image is numpy array, trajectory is a numpy array (x and y coordinates for mouse)
+    # plot the image,
+    # plot the trajectory as a line
+    # plot heatmap of the trajectory as a heatmap. 
+        # There is a function for this...
+    # Make sure coloring is consistent for all mice ... 
+    # Save this as an image in a output folder.
+ 
     
