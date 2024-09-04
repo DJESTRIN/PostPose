2e17c72bdc01f981c22181e29ac67f89c28096ce
David <david.james.estrin@outlook.com>
2024-09-04 16:39:53 -0400

    kenneth script added

--- core/kenneth_heatmap.py
+++ core/kenneth_heatmap.py
@@ -1,10 +1,14 @@
 
-def plot_heatmap(image,trajectory,outputfolder): #Image is numpy array, trajectory is a numpy array (x and y coordinates for mouse)
-    # plot the image,
+def plot_heatmap(image,trajectory,shape,outputfolder): #Image is numpy array, trajectory is a numpy array (x and y coordinates for mouse)
+    # Using matplotlib.pyplot do: ...
+    # plot the image
     # plot the trajectory as a line
     # plot heatmap of the trajectory as a heatmap. 
         # There is a function for this...
+    # plot shape if there is a shape...
     # Make sure coloring is consistent for all mice ... 
     # Save this as an image in a output folder.
+
+    return 
  
     
