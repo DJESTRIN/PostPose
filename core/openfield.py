913a2333053ca605baafd16057da3b4ad3b586bd
David <david.james.estrin@outlook.com>
2024-09-11 16:48:59 -0400

    updated openfield specific code

--- core/openfield.py
+++ core/openfield.py
@@ -7,11 +7,76 @@ Author: David Estrin
 Version: 1.0
 Date: 08-29-2024
 """
-
+from gestion import digestion
 from main import pipeline, delete_saved_objects
+from graphics import graphics, experimental_field
 import argparse
 import numpy as np
+import os
 import ipdb
+
+class openfield_graphics(graphics):
+    """ Openfield graphics ... uses graphics class to calculate the following details:
+        (1) Count number of times body (part) passes from inner to outer circle (vice versa). 
+            Default is average of all body parts.
+        (2) Percent of time body (part) spends inside inner circle versus outer circle. 
+            Default is average of all body parts.
+        (3) Average distance traveled, speed and acceleration magnitude of body (part) inside inner circle
+            versus outer circle. Default is average of all body parts.
+    """
+
+class openfield_pipeline(pipeline):
+    """ Updated pipeline class to accomodate openfield_graphics class """
+    def __call__(self):
+        """ Main set of steps for current analysis. """
+        # Loop over csv files, making sure all csv files have been processed.
+        self.digestion_objs = []
+        self.arena_objs = []
+
+        for csvfile in self.csv_files:
+            outputfile,_=csvfile.split('.cs')
+            outputfile+='.pkl'
+
+            # Determine if object was already created on a previous run.
+            if os.path.isfile(outputfile):
+                obj_oh = digestion.load(outputfile)
+            else:
+                obj_oh = digestion(csv_file=csvfile)
+                obj_oh()
+                obj_oh.save(outputfile)
+            
+            # Keep all digestion objects inside of a single attribute
+            self.digestion_objs.append(obj_oh)
+
+            # Get corresponding video file
+            video_file = self.match_csv_to_video(csvfile)
+
+            # Create or load corresponding arena object
+            field_file,_=re.split(r'\.avi|\.mp4', video_file)
+            field_file+='experimental_field.pkl'
+            if os.path.isfile(field_file):
+                arena_objoh = experimental_field.load(field_file)
+            else:
+                arena_objoh = experimental_field(input_video=video_file,
+                                           drop_directory=self.drop_directory,
+                                           shape_positions=self.shape_positions[0],
+                                           shapes=self.shapes[0])
+                arena_objoh.save(field_file)
+        
+            # Keep all arena objects inside a single attribute
+            self.arena_objs.append(arena_objoh)
+
+            # Create or load graphics object
+            graphics_file,_=re.split(r'\.avi|\.mp4', video_file)
+            graphics_file+='graphics.pkl'
+            if os.path.isfile(graphics_file):
+                graph_obj = graphics.load(graphics_file)
+            else:
+                graph_obj = graphics(digested_obj=obj_oh,
+                                    arena_obj=arena_objoh,
+                                    drop_directory=self.drop_directory)
+                graph_obj()
+                graph_obj.save(field_file)
 
 def generate_openfield_shapes(input_circle_shape,input_shape_string,percent=0.75):
     """ Generate openfield shapes """
